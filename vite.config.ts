4bb5b74e422e29288b6ef481c05abfe124dae06d
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-01-10 19:26:27 +0900

    chore: Viteの設定ファイルの型エラーを抑制

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
+	// @ts-expect-error https://github.com/sveltejs/kit/issues/13102#issuecomment-2515203461
 	plugins: [sveltekit()],
 
 	test: {
